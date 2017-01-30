e74767314a5ffeffc700e2b710accccc4594f6a2
yame <yame@free.fr>
2017-01-30 21:38:53 +0000

    Add cdrom tray status program

---
+++ trayopen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <linux/cdrom.h>
+
+int main(int argc,char **argv) {
+  int cdrom;
+  int status=1;
+
+/* gcc -o trayopen trayopen.c */
+/* Print Open if cd tray is open */
+
+  if (! argv[1] ){
+    printf("Usage : trayopen [device]\n");
+    printf("Result: Returns a 0 if the tray was open and 1 if it was closed\n");
+    exit(2);
+  }
+
+  if ((cdrom = open(argv[1],O_RDONLY | O_NONBLOCK)) < 0) {
+    printf("Unable to open device %s. Provide a device name (/dev/sr0, /dev/cdrom) as a parameter.\n",argv[1]);
+    exit(2);
+  }
+  /* Check CD tray status */
+  if (ioctl(cdrom,CDROM_DRIVE_STATUS) == CDS_TRAY_OPEN) {
+    status=0;
+   // printf("Open\n");
+  }
+  close(cdrom);
+  exit(status);
+}
