1b794a3d9dd457a50ba6dbd02d6ee24dddb782ea
yame <yame@free.fr>
2017-02-08 17:32:33 +0000

    Check sound card output

--- usr/local/bin/SetPhono.sh
+++ usr/local/bin/SetPhono.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-arecord -d hw:1,0 | aplay -d hw:0,0 &  
+if aplay -l | grep "OutPlayer" | grep "card 0" &>/dev/null; then
+	#echo "hw:0,0"
+	arecord -d hw:1,0 | aplay -d hw:0,0 &  
+else
+	#echo "hw:1,0"
+	arecord -d hw:0,0 | aplay -d hw:1,0 &  
+fi
