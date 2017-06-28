bf9c5bd831c155c454d5d0a80f8feb67b9c60ae5
yame <yame@free.fr>
2017-06-28 16:07:58 +0000

    Update checkcd.sh script

--- usr/local/bin/checkcd.sh
+++ usr/local/bin/checkcd.sh
@@ -5,40 +5,42 @@ log()
 	logger -t ProcessIO $1
 }
 
-status=1;
+STATUS=1;
 
-while [ $status -ne 0 ]; do
+while [ $STATUS -ne 0 ]; do
 sleep 0.5
 checkdisc /dev/cdrom
+RET=$?
+	if [ $RET -eq 10 ]; then
+		log  "No disc"
+		WriteInfo.sh "nodisc"
+		if [ -e /ramtmp/toc ]; then
+			rm /ramtmp/toc
+		fi
+		STATUS=0;
+	fi
 
-if [ $? -eq 10 ]; then
-log  "No disc"
-WriteInfo.sh "nodisc"
-rm /ramtmp/toc
-status=0;
-fi
+	if [ $RET -eq 15 ]; then
+		log  "No disc info"
+	fi
 
-if [ $? -eq 15 ]; then
-log  "No disc info"
-fi
+	if [ $RET -eq 20 ]; then
+		log  "Tray open"
+		WriteInfo.sh "open"
+	fi
 
-if [ $? -eq 20 ]; then
-log  "Tray open"
-WriteInfo.sh "open"
-fi
+	if [ $RET -eq 25 ]; then
+		log  "Drive not ready"
+		WriteInfo.sh "wait"
+	fi
 
-if [ $? -eq 25 ]; then
-log  "Drive not ready"
-WriteInfo.sh "wait"
-fi
-
-if [ $? -eq 30 ]; then
-log  "disc ok"
-export HOME=/root/
-cdtoc.exp > /dev/null 2>&1
-cdcd tracks > /ramtmp/toc
-WriteInfo.sh "discok"
-status=0;
-fi
+	if [ $RET -eq 30 ]; then
+		log  "disc ok"
+		export HOME=/root/
+		cdtoc.exp > /dev/null 2>&1
+		cdcd tracks > /ramtmp/toc
+		WriteInfo.sh "discok"
+		STATUS=0;
+	fi
 
 done
