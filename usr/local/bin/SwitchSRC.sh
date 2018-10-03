69f2949ea381958b267fc190206d701375a222b1
yame <yame@free.fr>
2018-10-03 22:05:34 +0000

    Update cd management

--- usr/local/bin/SwitchSRC.sh
+++ usr/local/bin/SwitchSRC.sh
@@ -3,29 +3,33 @@
 
 log()
 {
-	logger -t AudioService $1
+	logger -t SwitchSource $1
 }
 
 if [ -f /etc/cdplayer/phono ]; then
+	WriteInfo.sh -s "phono"
         PCM1792AMute.sh OFF
 	DaemonPhono.sh &
-	WriteInfo.sh -s "phono"
 elif [ -f /etc/cdplayer/spdif1 ]; then
+	WriteInfo.sh -s "spdif1"
         PCM1792AMute.sh ON
         pkill DaemonPhono.sh
-	WriteInfo.sh -s "spdif1"
 elif [ -f /etc/cdplayer/spdif2 ]; then
 	WriteInfo.sh -s "spdif2"
 elif [ -f /etc/cdplayer/analog ]; then
 	WriteInfo.sh -s "anlg"
 elif [ -f /etc/cdplayer/cdplayer ]; then
+	WriteInfo.sh -s "cd"
         LockCmd ON
+        log "lock command input"
         ResetAudio.sh
+        log "Hardware reset done"
         sleep 3
         DriveCD.sh -l
+        log "load and check cd"
         LockCmd OFF
+        log "unlock command input"
         PCM1792AMute.sh OFF
-	WriteInfo.sh -s "cd"
 elif [ -f /etc/cdplayer/mpd ]; then
 	WriteInfo.sh -s "mpd"
 fi
--- usr/local/bin/checkcd.sh
+++ usr/local/bin/checkcd.sh
@@ -41,7 +41,7 @@ RET=$?
 		log  "Retrieve TOC disc"
 		cdcd tracks  > /ramtmp/toc
 		log  "Copy TOC on /ramtmp/toc"
-		WriteInfo.sh -r "discok"
+                WriteInfo.sh -l "$(ParseTOC.py -a) $(ParseTOC.py -n)"
 		STATUS=0;
 	fi
 
