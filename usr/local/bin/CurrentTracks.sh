12c351db5620b95547dfb5c3a9c904ca6b4bc38f
yame <yame@free.fr>
2019-03-04 22:33:35 +0000

    get cd position : Work in progress

--- usr/local/bin/CurrentTracks.sh
+++ usr/local/bin/CurrentTracks.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 CDPLOG="/ramtmp/mplayer.log"
 CDCTRL="/ramtmp/cdcontrol"
+CDPLAY="/ramtmp/CDisPlaying"
 
-echo "" > $CDPLOG
-echo "get_time_pos" > $CDCTRL
-sleep 0.08
-CPOS=$(grep -a "POSITION=" $CDPLOG | cut -d"=" -f2)
-CPOS=$(echo "(($CPOS * 75)+0.5)/1" | bc)
-echo $CPOS
+if [ -f $CDPLAY ]; then
+    TOTAL=$(ParseTOC.py -b)
+    OFFSET=$(ParseTOC.py -d 1 | cut -d" " -f1)
+    echo $OFFSET
+    #OFFSET=$(echo "$OFFSET/75" | bc)
+    #echo $OFFSET
+    echo "" > $CDPLOG
+    echo "get_time_pos" > $CDCTRL
+    sleep 0.08
+    CPOS=$(grep -a "POSITION=" $CDPLOG | cut -d"=" -f2)
+    CPOS=$(echo "(($CPOS * 75)+0.5)/1" | bc)
+    echo $CPOS
+    for i in `seq 1 $TOTAL`; do
+        STARTCT=$(ParseTOC.py -d $i | cut -d" " -f1)
+        STOPCT=$(ParseTOC.py -d $i | cut -d" " -f2)
+        echo $STARTCT
+        echo $STOPCT
+    done
+else 
+    echo "no cd playing"
+fi
